f062dcdc6eb29eea1fa5b1c1647d36ffcf2fbd9d
Norman Cade <norman_cade32@yahoo.com>
2023-05-17 17:04:55 -0400

    Header section is complete

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import "./Header.css";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app_wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavor" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid explicabo quisquam iusto
+        nisi delectus provident blanditiis enim atque quod fugit autem aspernatur sequi facilis
+        nesciunt, debitis in magni ducimus repudiandae.
+      </p>
+      <button className="custom__button" type="button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="welcome" />
+    </div>
   </div>
 );
 
