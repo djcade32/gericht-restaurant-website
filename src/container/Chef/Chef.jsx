837bfbd0666ceefd2095f0d795a3b3bfe66e65b1
Norman Cade <norman_cade32@yahoo.com>
2023-05-22 15:34:48 -0400

    Chef section complete

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import "./Chef.css";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="qutoe" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, labore.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis animi excepturi molestiae
+          corporis explicabo debitis inventore ipsam dicta accusantium laboriosam amet vitae at
+          impedit quis atque ratione a officiis rem, dolore reiciendis, quia facilis reprehenderit
+          accusamus! Non expedita ratione ipsum.
+        </p>
+      </div>
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
